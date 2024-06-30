a7ca15a7e2c5772198d7ff6947212849e873353f
Mackenzie F Libby <mackenzieflibby@gmail.com>
2024-06-30 06:23:19 -0400

    base implementation

---
+++ Library.fs
@@ -0,0 +1,17 @@
+﻿namespace HCRD
+
+module CLIArgs =
+    open System.Text.RegularExpressions
+
+
+
+    let private re = Regex("""(-{1,2}[\S]+)[ ]*([^\r\n\t\f\v "-]+|"[\S ]+"){0,1}""", RegexOptions.Compiled)
+
+    let parseArgs (args : string []) =
+        re.Matches(String.concat " " args)
+        |> Seq.map 
+            (fun mch ->
+            //expect one XOR two groups, never more:
+                let groups = mch.Groups 
+                (Seq.head groups, Seq.head <| Seq.tail groups) 
+            ) 
